aae36afd33c95d24c00bd40942b3fd7f5fd93fa6
Takuya Mizokami <takoyaki65@users.noreply.github.com>
2024-09-18 17:08:26 +0900

    DBスキーマの変更に伴ってpymysqlのデータモデル定義を修正

--- app/classes/models.py
+++ app/classes/models.py
@@ -1,80 +1,217 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Boolean
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    DateTime,
+    ForeignKey,
+    Boolean,
+    Enum,
+    text,
+)
 from sqlalchemy.orm import declarative_base, relationship
 
 
 Base = declarative_base()
 
 
-class Assignment(Base):
-    __tablename__ = "assignments"
+class Lecture(Base):
+    __tablename__ = "Lecture"
     id = Column(Integer, primary_key=True, autoincrement=True)
     title = Column(String(255), nullable=False)
-    test_dir_name = Column(String(255), nullable=False)
     start_date = Column(DateTime, nullable=False)
     end_date = Column(DateTime, nullable=False)
-    sub_assignments = relationship("SubAssignment", back_populates="assignment")
+    problems = relationship("Problem", back_populates="lecture")
 
 
-class SubAssignment(Base):
-    __tablename__ = "sub_assignments"
-    id = Column(Integer, ForeignKey("assignments.id"), primary_key=True)
-    sub_id = Column(Integer, primary_key=True)
+class Problem(Base):
+    __tablename__ = "Problem"
+    lecture_id = Column(
+        Integer, ForeignKey("Lecture.id"), primary_key=True, nullable=False
+    )
+    assignment_id = Column(Integer, primary_key=True, nullable=False)
+    for_evaluation = Column(Boolean, primary_key=True, nullable=False)
     title = Column(String(255), nullable=False)
-    test_dir_name = Column(String(255), nullable=False)
-    makefile = Column(String(255), nullable=False)
-    required_file_name = Column(String(255), nullable=False)
-    main_file_name = Column(String(255), nullable=False)
-    test_case_name = Column(String(255), nullable=True)
-    assignment = relationship("Assignment", back_populates="sub_assignments")
+    description_path = Column(String(255), nullable=False)
+    timeMS = Column(Integer, nullable=False)
+    memoryMB = Column(Integer, nullable=False)
+    lecture = relationship("Lecture", back_populates="problems")
 
 
-class User(Base):
-    __tablename__ = "users"
+class Executables(Base):
+    __tablename__ = "Executables"
     id = Column(Integer, primary_key=True, autoincrement=True)
-    student_id = Column(String(255), nullable=False)
+    lecture_id = Column(Integer, ForeignKey("Problem.lecture_id"))
+    assignment_id = Column(Integer, ForeignKey("Problem.assignment_id"))
+    for_evaluation = Column(Boolean, ForeignKey("Problem.for_evaluation"))
+    name = Column(String(255), nullable=False)
+
+
+class ArrangedFiles(Base):
+    __tablename__ = "ArrangedFiles"
+    str_id = Column(String(255), primary_key=True)
+    lecture_id = Column(Integer, ForeignKey("Problem.lecture_id"))
+    assignment_id = Column(Integer, ForeignKey("Problem.assignment_id"))
+    for_evaluation = Column(Boolean, ForeignKey("Problem.for_evaluation"))
+    path = Column(String(255), nullable=False)
+
+
+class RequiredFiles(Base):
+    __tablename__ = "RequiredFiles"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    lecture_id = Column(Integer, ForeignKey("Problem.lecture_id"))
+    assignment_id = Column(Integer, ForeignKey("Problem.assignment_id"))
+    for_evaluation = Column(Boolean, ForeignKey("Problem.for_evaluation"))
+    name = Column(String(255), nullable=False)
+
+
+class EvaluationItems(Base):
+    __tablename__ = "EvaluationItems"
+    str_id = Column(String(255), primary_key=True)
+    lecture_id = Column(Integer, ForeignKey("Problem.lecture_id"))
+    assignment_id = Column(Integer, ForeignKey("Problem.assignment_id"))
+    for_evaluation = Column(Boolean, ForeignKey("Problem.for_evaluation"))
+    title = Column(String(255), nullable=False)
+    description = Column(String)
+    score = Column(Integer, nullable=False)
+    type = Column(Enum("Built", "Judge"), nullable=False)
+    arranged_file_id = Column(String(255), ForeignKey("ArrangedFiles.str_id"))
+    message_on_fail = Column(String(255))
+
+
+class TestCases(Base):
+    __tablename__ = "TestCases"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    eval_id = Column(String(255), ForeignKey("EvaluationItems.str_id"), nullable=False)
+    description = Column(String)
+    command = Column(String(255), nullable=False)
+    argument_path = Column(String(255))
+    stdin_path = Column(String(255))
+    stdout_path = Column(String(255))
+    stderr_path = Column(String(255))
+    exit_code = Column(Integer, nullable=False, default=0)
+
+
+class Users(Base):
+    __tablename__ = "Users"
+    user_id = Column(String(255), primary_key=True)
     username = Column(String(255), nullable=False)
     email = Column(String(255), nullable=False)
     hashed_password = Column(String(255), nullable=False)
-    is_admin = Column(Boolean, default=False)
-    disabled = Column(Boolean, default=False)
-    created_at = Column(DateTime, nullable=False)
-    updated_at = Column(DateTime, nullable=True)
-    active_start_date = Column(DateTime, nullable=True)
-    active_end_date = Column(DateTime, nullable=True)
-
-    access_tokens = relationship("AccessToken", back_populates="user")
-    refresh_tokens = relationship("RefreshToken", back_populates="user")
-
-    class Config:
-        orm_mode = True
+    is_admin = Column(Boolean, default=False, nullable=False)
+    disabled = Column(Boolean, default=False, nullable=False)
+    created_at = Column(DateTime, server_default=text("CURRENT_TIMESTAMP"))
+    updated_at = Column(
+        DateTime,
+        server_default=text("CURRENT_TIMESTAMP"),
+        onupdate=text("CURRENT_TIMESTAMP"),
+    )
+    active_start_date = Column(DateTime, default=None)
+    active_end_date = Column(DateTime, default=None)
 
 
-class AccessToken(Base):
-    __tablename__ = "access_tokens"
+class LoginHistory(Base):
+    __tablename__ = "LoginHistory"
+    user_id = Column(
+        String(255), ForeignKey("Users.user_id"), primary_key=True, nullable=False
+    )
+    login_at = Column(DateTime, nullable=False)
+    logout_at = Column(DateTime, default=None)
+    refresh_count = Column(Integer, default=0, nullable=False)
+    current_access_token = Column(String(255), nullable=False)
+    current_refresh_token = Column(String(255), nullable=False)
+
+
+class BatchSubmission(Base):
+    __tablename__ = "BatchSubmission"
     id = Column(Integer, primary_key=True, autoincrement=True)
-    token = Column(String(255), nullable=False)
-    expired_at = Column(DateTime, nullable=False)
-    is_expired = Column(Boolean, default=False)
-    user_id = Column(Integer, ForeignKey("users.id"), nullable=True)
-
-    user = relationship("User", back_populates="access_tokens")
+    ts = Column(DateTime, server_default=text("CURRENT_TIMESTAMP"))
+    user_id = Column(String(255), ForeignKey("Users.user_id"))
 
 
-class RefreshToken(Base):
-    __tablename__ = "refresh_tokens"
+class Submission(Base):
+    __tablename__ = "Submission"
     id = Column(Integer, primary_key=True, autoincrement=True)
-    token = Column(String(255), nullable=False)
-    expired_at = Column(DateTime, nullable=False)
-    is_expired = Column(Boolean, default=False)
-    user_id = Column(Integer, ForeignKey("users.id"), nullable=True)
+    ts = Column(DateTime, server_default=text("CURRENT_TIMESTAMP"))
+    batch_id = Column(Integer, ForeignKey("BatchSubmission.id"))
+    user_id = Column(String(255), ForeignKey("Users.user_id"), nullable=False)
+    lecture_id = Column(Integer, ForeignKey("Problem.lecture_id"), nullable=False)
+    assignment_id = Column(Integer, ForeignKey("Problem.assignment_id"), nullable=False)
+    for_evaluation = Column(
+        Boolean, ForeignKey("Problem.for_evaluation"), nullable=False
+    )
+    progress = Column(Enum("pending", "queued", "running", "done"), default="pending")
+    total_task = Column(Integer, nullable=False, default=0)
+    completed_task = Column(Integer, nullable=False, default=0)
 
-    user = relationship("User", back_populates="refresh_tokens")
 
-
-class FunctionTest(Base):
-    __tablename__ = "function_test"
+class UploadedFiles(Base):
+    __tablename__ = "UploadedFiles"
     id = Column(Integer, primary_key=True, autoincrement=True)
-    sub_id = Column(Integer, primary_key=True)
-    func_id = Column(Integer, primary_key=True)
-    func_name = Column(String(255), nullable=False)
-    exec_command = Column(String(255), nullable=True)
+    ts = Column(DateTime, server_default=text("CURRENT_TIMESTAMP"))
+    submission_id = Column(Integer, ForeignKey("Submission.id"))
+    path = Column(String(255), nullable=False)
+
+
+class JudgeResult(Base):
+    __tablename__ = "JudgeResult"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    ts = Column(DateTime, server_default=text("CURRENT_TIMESTAMP"))
+    submission_id = Column(Integer, ForeignKey("Submission.id"))
+    testcase_id = Column(Integer, ForeignKey("TestCases.id"))
+    result = Column(
+        Enum("AC", "WA", "TLE", "MLE", "RE", "CE", "OLE", "IE"), nullable=False
+    )
+    timeMS = Column(Integer, nullable=False)
+    memoryKB = Column(Integer, nullable=False)
+    exit_code = Column(Integer, nullable=False)
+    stdout = Column(String, nullable=False)
+    stderr = Column(String, nullable=False)
+    description = Column(String)
+    command = Column(String, nullable=False)
+    stdin = Column(String)
+    expected_stdout = Column(String)
+    expected_stderr = Column(String)
+    expected_exit_code = Column(Integer, nullable=False, default=0)
+
+
+class EvaluationSummary(Base):
+    __tablename__ = "EvaluationSummary"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    submission_id = Column(Integer, ForeignKey("Submission.id"), nullable=False)
+    batch_id = Column(Integer, ForeignKey("BatchSubmission.id"))
+    user_id = Column(String(255), ForeignKey("Users.user_id"), nullable=False)
+    lecture_id = Column(Integer, ForeignKey("Problem.lecture_id"), nullable=False)
+    assignment_id = Column(Integer, ForeignKey("Problem.assignment_id"), nullable=False)
+    for_evaluation = Column(
+        Boolean, ForeignKey("Problem.for_evaluation"), nullable=False
+    )
+    eval_id = Column(String(255), ForeignKey("EvaluationItems.str_id"), nullable=False)
+    arranged_file_id = Column(String(255), ForeignKey("ArrangedFiles.str_id"))
+    result = Column(
+        Enum("AC", "WA", "TLE", "MLE", "RE", "CE", "OLE", "IE"), nullable=False
+    )
+    message = Column(String(255))
+    detail = Column(String(255))
+    score = Column(Integer, nullable=False)
+    eval_title = Column(String(255), nullable=False)
+    eval_description = Column(String)
+    eval_type = Column(Enum("Built", "Judge"), nullable=False)
+    arranged_file_path = Column(String(255))
+
+
+class SubmissionSummary(Base):
+    __tablename__ = "SubmissionSummary"
+    submission_id = Column(Integer, ForeignKey("Submission.id"), primary_key=True)
+    batch_id = Column(Integer, ForeignKey("BatchSubmission.id"))
+    user_id = Column(String(255), ForeignKey("Users.user_id"))
+    lecture_id = Column(Integer, ForeignKey("Problem.lecture_id"), nullable=False)
+    assignment_id = Column(Integer, ForeignKey("Problem.assignment_id"), nullable=False)
+    for_evaluation = Column(
+        Boolean, ForeignKey("Problem.for_evaluation"), nullable=False
+    )
+    result = Column(
+        Enum("AC", "WA", "TLE", "MLE", "RE", "CE", "OLE", "IE", "FN"), nullable=False
+    )
+    message = Column(String(255))
+    detail = Column(String(255))
+    score = Column(Integer, nullable=False)
