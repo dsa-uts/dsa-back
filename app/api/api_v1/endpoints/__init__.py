4dd46e482780c3a5a20794896f60f63a1e40552e
Takuya Mizokami <takoyaki65@users.noreply.github.com>
2024-09-22 12:37:07 +0900

    コメント追加

--- app/api/api_v1/endpoints/__init__.py
+++ app/api/api_v1/endpoints/__init__.py
@@ -3,6 +3,7 @@ from . import assignments
 from . import authorize
 from . import users
 
+################### /api/v1/... 以下のエンドポイントの定義 ###################
 api_router = APIRouter()
 api_router.include_router(
     assignments.router, prefix="/assignments", tags=["assignments"]
@@ -11,3 +12,4 @@ api_router.include_router(
 api_router.include_router(authorize.router, prefix="/authorize", tags=["authorize"])
 
 api_router.include_router(users.router, prefix="/users", tags=["users"])
+##########################################################################
