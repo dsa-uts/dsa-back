6b7b566276f5b6f922f03c326e24b2146bd5006f
Takuya Mizokami <takoyaki65@users.noreply.github.com>
2024-09-23 16:55:17 +0900

    ジャッジ提出のためのCRUD関数を追加

--- app/crud/db/assignments.py
+++ app/crud/db/assignments.py
@@ -4,6 +4,7 @@ from ...classes import models
 from typing import List
 from datetime import datetime
 import pytz
+from pathlib import Path
 
 
 def get_lecture_list(db: Session) -> List[schemas.LectureRecord]:
@@ -70,6 +71,7 @@ def get_evaluation_item_list(
         for evaluation_item in evaluation_item_list
     ]
 
+
 def get_evaluation_item(
     db: Session, eval_id: str
 ) -> schemas.EvaluationItemRecord | None:
@@ -81,21 +83,23 @@ def get_evaluation_item(
         .filter(models.EvaluationItems.str_id == eval_id)
         .first()
     )
-    return schemas.EvaluationItemRecord(**evaluation_item.__dict__) if evaluation_item is not None else None
+    return (
+        schemas.EvaluationItemRecord(**evaluation_item.__dict__)
+        if evaluation_item is not None
+        else None
+    )
 
 
-def get_test_case_list(
-    db: Session, eval_id: str
-) -> list[schemas.TestCaseRecord]:
+def get_test_case_list(db: Session, eval_id: str) -> list[schemas.TestCaseRecord]:
     """
     特定の評価項目に基づくテストケースのリストを取得する関数
     """
     test_case_list = (
-        db.query(models.TestCases)
-        .filter(models.TestCases.eval_id == eval_id)
-        .all()
+        db.query(models.TestCases).filter(models.TestCases.eval_id == eval_id).all()
     )
-    return [schemas.TestCaseRecord(**test_case.__dict__) for test_case in test_case_list]
+    return [
+        schemas.TestCaseRecord(**test_case.__dict__) for test_case in test_case_list
+    ]
 
 
 def get_problem_recursive(
@@ -103,7 +107,7 @@ def get_problem_recursive(
 ) -> schemas.ProblemRecord:
     """
     特定の授業の特定の課題のエントリを取得する関数
-    
+
     課題に紐づく評価項目リストと、評価項目に紐づくテストケースリストも合わせて取得する
     """
     problem = (
@@ -111,14 +115,16 @@ def get_problem_recursive(
         .filter(
             models.Problem.lecture_id == lecture_id,
             models.Problem.assignment_id == assignment_id,
-            models.Problem.for_evaluation == for_evaluation
+            models.Problem.for_evaluation == for_evaluation,
         )
         .first()
     )
     problem_record = schemas.ProblemRecord(**problem.__dict__)
     # problem_record.evaluation_item_listを取得する
-    problem_record.evaluation_item_list = get_evaluation_item_list(db, lecture_id, assignment_id, for_evaluation)
-    
+    problem_record.evaluation_item_list = get_evaluation_item_list(
+        db, lecture_id, assignment_id, for_evaluation
+    )
+
     # evaluation_item_listの各eval_idに基づくtest_case_listを取得する
     for evaluation_item in problem_record.evaluation_item_list:
         evaluation_item.testcase_list = get_test_case_list(db, evaluation_item.str_id)
@@ -126,7 +132,7 @@ def get_problem_recursive(
 
 
 def get_required_files(
-    db: Session, lecture_id: int, assignment_id: int, for_evaluation: bool 
+    db: Session, lecture_id: int, assignment_id: int, for_evaluation: bool
 ) -> list[str]:
     """
     特定の課題に基づく必要ファイルのリストを取得する関数
@@ -136,7 +142,7 @@ def get_required_files(
         .filter(
             models.RequiredFiles.lecture_id == lecture_id,
             models.RequiredFiles.assignment_id == assignment_id,
-            models.RequiredFiles.for_evaluation == for_evaluation
+            models.RequiredFiles.for_evaluation == for_evaluation,
         )
         .all()
     )
@@ -154,8 +160,81 @@ def get_arranged_filepaths(
         .filter(
             models.ArrangedFiles.lecture_id == lecture_id,
             models.ArrangedFiles.assignment_id == assignment_id,
-            models.ArrangedFiles.for_evaluation == for_evaluation
+            models.ArrangedFiles.for_evaluation == for_evaluation,
         )
         .all()
     )
-    return [schemas.ArrangedFileRecord(**arranged_file.__dict__) for arranged_file in arranged_files]
+    return [
+        schemas.ArrangedFileRecord(**arranged_file.__dict__)
+        for arranged_file in arranged_files
+    ]
+
+
+def register_submission(
+    db: Session,
+    batch_id: int | None,
+    user_id: str,
+    lecture_id: int,
+    assignment_id: int,
+    for_evaluation: bool,
+) -> schemas.SubmissionRecord:
+    """
+    ジャッジリクエストをSubmissionテーブルに登録する関数
+    """
+    new_submission = models.Submission(
+        batch_id=batch_id,
+        user_id=user_id,
+        lecture_id=lecture_id,
+        assignment_id=assignment_id,
+        for_evaluation=for_evaluation,
+    )
+    db.add(new_submission)
+    db.commit()
+    db.refresh(new_submission)
+    return schemas.SubmissionRecord(**new_submission.__dict__)
+
+
+def get_submission(db: Session, submission_id: int) -> schemas.SubmissionRecord | None:
+    """
+    特定の提出エントリを取得する関数
+    """
+    submission = (
+        db.query(models.Submission)
+        .filter(models.Submission.id == submission_id)
+        .first()
+    )
+    return (
+        schemas.SubmissionRecord(**submission.__dict__)
+        if submission is not None
+        else None
+    )
+
+
+def modify_submission(db: Session, submission_record: schemas.SubmissionRecord) -> None:
+    """
+    提出エントリを更新する関数
+    """
+    db.query(models.Submission).filter(
+        models.Submission.id == submission_record.id
+    ).update(submission_record.model_dump())
+    db.commit()
+
+
+def register_uploaded_file(db: Session, submission_id: int, path: Path) -> None:
+    """
+    アップロードされたファイルをUploadedFilesテーブルに登録する関数
+    """
+    new_uploadedfile = models.UploadedFiles(submission_id=submission_id, path=str(path))
+    db.add(new_uploadedfile)
+    db.commit()
+
+
+def register_batch_submission(db: Session, user_id: str) -> schemas.BatchSubmissionRecord:
+    """
+    バッチ提出をBatchSubmissionテーブルに登録する関数
+    """
+    new_batch_submission = models.BatchSubmission(user_id=user_id)
+    db.add(new_batch_submission)
+    db.commit()
+    db.refresh(new_batch_submission)
+    return schemas.BatchSubmissionRecord(**new_batch_submission.__dict__)
